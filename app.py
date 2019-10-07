3a2271052460e997e32288ae00eda64022eb1287
Ubuntu <ubuntu@ip-172-31-47-213.ec2.internal>
2019-10-07 11:24:29 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'recipe_catalog'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-pwjgy.mongodb.net/recipe_catalog?retryWrites=true&w=majority'
+#app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_catalog')
+def get_catalog():
+    return render_template("catalog.html", recipes=mongo.db.tasks.find())
