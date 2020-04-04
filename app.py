a3a45298bf9c6415a64213e5ce7a1297ac802043
eldowling <elinordowling@gmail.com>
2020-04-04 12:48:39 +0000

    Adding user log in using bcrypt password encoding

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session
+#from flask.ext.bcrypt import Bcrypt
+#import flask_bcrypt
+import bcrypt
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
-from flask.ext.pymongo import PyMongo
-import bcrypt
 app = Flask(__name__)
+#bcrypt = Bcrypt(app)
+#bcrypt = flask_bcrypt.Bcrypt(app)
+#mongo = PyMongo(app)
 app.config["MONGO_DBNAME"] = 'recipie_catalog'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-pwjgy.mongodb.net/recipie_catalog?retryWrites=true&w=majority'
 #app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
@@ -40,11 +44,27 @@ def login():
     login_user = users.find_one({'name' : request.form['username']})
 
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
             session['username'] = request.form['username']
             return redirect(url_for('get_catalog'))
 
     return 'Invalid username/password combination'
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name' : request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({'name' : request.form['username'], 'password' : hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        
+        return 'That username already exists!'
+
+    return render_template('register.html')
      
 @app.route('/show_recipe/<recipe_id>')
 def show_recipe(recipe_id):
@@ -110,6 +130,7 @@ def maintenance():
     
     
 if __name__ == '__main__':
+    app.secret_key = 'mysecret'
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
